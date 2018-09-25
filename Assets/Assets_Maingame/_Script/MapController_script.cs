ce35ebc97911baf37bd38bda8ae44d51939a248e
zvzv1919 <zvzv1919@gmail.com>
2018-09-25 16:52:24 -0400

    bug fixed

---
+++ Assets/Assets_Maingame/_Script/MapController_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MapController_script : MonoBehaviour {
+
+    //for prototyping purposes
+    public ArrayList monsterHolder;
+
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
