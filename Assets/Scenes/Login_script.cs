6f3f8358f5a74117d4d219fa0993a20f17a90cd9
nicholasmeier <nmeier@purdue.edu>
2018-10-02 19:00:04 -0400

    Added firebase sdk, tried to add scripts to login screen buttons

---
+++ Assets/Scenes/Login_script.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login_script : MonoBehaviour {
+
+    public InputField userField;
+    public InputField passField;
+    public static string username;
+    public static string pass;
+
+    public Button loginButton;
+
+	// Use this for initialization
+	void Start () {
+        Button lgn_btn = loginButton.GetComponent<Button>();
+
+        lgn_btn.onClick.AddListener(login_check);
+
+	}
+	
+    void login_check()
+    {
+        username = userField.text.ToString();
+        Debug.Log("df;lahjfka");
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
