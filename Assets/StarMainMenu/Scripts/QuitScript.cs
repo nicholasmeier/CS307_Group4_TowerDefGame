3ad4b3fc042eecc3aa3db1446550e03e33626e19
Xihao li <li2583@purdue.edu>
2018-10-02 18:02:02 -0400

    test_commit_startmenu_finished

---
+++ Assets/StarMainMenu/Scripts/QuitScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour
+{
+
+    public void quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.quit();
+#endif
+    }
+}
