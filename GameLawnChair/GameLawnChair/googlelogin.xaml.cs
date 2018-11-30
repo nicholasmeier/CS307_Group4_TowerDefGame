5b14977b723b511be95ed6b79b5534f75b4e37a0
nicholasmeier <nmeier@purdue.edu>
2018-11-30 14:43:20 -0500

    Progress on google

--- GameLawnChair/GameLawnChair/googlelogin.xaml.cs
+++ GameLawnChair/GameLawnChair/googlelogin.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,6 +12,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util;
+using Firebase;
+using Firebase.Auth;
+
 
 namespace GameLawnChair
 {
@@ -19,6 +25,10 @@ namespace GameLawnChair
     /// </summary>
     public partial class Window1 : Window
     {
+        public const string FirebaseAppKey = "AIzaSyAiX70y-obo-vUyAO7-LJM8EwGcRD2UEWg";
+        public const string FirebaseAppUrl = "https://cs-307-herotd.firebaseapp.com/";
+        public const string GoogleID = "326999430623-3sqk83o24q6rdfhods04vkmrc72e3api.apps.googleusercontent.com";
+
         public Window1()
         {
             InitializeComponent();
@@ -42,6 +52,31 @@ namespace GameLawnChair
             var win = new MainWindow();
             win.Show();
             this.Close();
+        }
+
+        private async void GoogleClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var cr = new PromptCodeReceiver();
+
+                var result = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    new ClientSecrets { ClientId = GoogleID },
+                    new[] { "email", "profile" },
+                    "user",
+                    CancellationToken.None);
+
+                if (result.Token.IsExpired(SystemClock.Default))
+                {
+                    await result.RefreshTokenAsync(CancellationToken.None);
+                }
+
+                //this.FetchFirebaseData(result.Token.AccessToken, FirebaseAuthType.Google);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
